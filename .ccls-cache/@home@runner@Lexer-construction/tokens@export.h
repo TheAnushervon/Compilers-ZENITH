452630a828b6092c9076be38a09c70209ec1b513
VechkanovVV <hatavidit84@gmail.com>
2024-12-02 22:02:15 +0300

    delete

--- .ccls-cache/@home@runner@Lexer-construction/tokens@export.h
+++
@@ -1,9 +0,0 @@
-// export.h
-#ifndef EXPORT_H
-#define EXPORT_H
-
-#include "enums/token_type.h"
-#include "pars/pars_functions.h"
-#include "structs/token.h"
-
-#endif // EXPORT_H
--- .ccls-cache/@home@runner@Lexer-construction/tokens@structs@token.cpp
+++
@@ -1,7 +0,0 @@
-#include "../enums/token_type.cpp"
-
-struct Token {
-   TokenType type;
-   std::string value;
- Token(TokenType t, const std::string& v) : type(t), value(v) {}
-};
